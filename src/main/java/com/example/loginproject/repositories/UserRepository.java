45f3613c530172ccbf6eb5503aa111b9ac5a9c41
Kauan Jasiskis <jasiskiskauan@gmail.com>
2024-06-18 12:15:43 -0300

    security filter and jwtToken feat

---
+++ src/main/java/com/example/loginproject/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.loginproject.repositories;
+
+import com.example.loginproject.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UserRepository extends JpaRepository<User,String>  {
+
+    UserDetails findByLogin(String login);
+
+}
---
+++ src/main/java/com/example/loginproject/security/TokenService.java
@@ -0,0 +1,55 @@
+package com.example.loginproject.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.example.loginproject.domain.User;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+
+    @Value("${api.security.token.secret}")
+    private String secret;
+
+    public String generateToken(User user){
+        try{
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            String token = JWT.create()
+                    .withIssuer("login-project")
+                    .withSubject(user.getLogin())
+                    .withExpiresAt(getExpirationDate())
+                    .sign(algorithm);
+            return token;
+        }catch (JWTCreationException exception){
+            throw new RuntimeException("Erro na geracao do token",exception);
+        }
+
+    }
+
+    public String validateToken(String token){
+        try{
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("login-project")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        }catch (JWTVerificationException exception){
+            return "";
+        }
+    }
+
+
+
+    public Instant getExpirationDate(){
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+
+}
